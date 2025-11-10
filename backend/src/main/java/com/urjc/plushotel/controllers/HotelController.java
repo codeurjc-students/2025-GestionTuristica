dda0554621155907b7fb37d2a111281ccc27d7f0
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2025-11-10 11:35:29 +0100

    Implement basic Hotel Controller

---
+++ backend/src/main/java/com/urjc/plushotel/controllers/HotelController.java
@@ -0,0 +1,29 @@
+package com.urjc.plushotel.controllers;
+
+import com.urjc.plushotel.entities.Hotel;
+import com.urjc.plushotel.services.HotelService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
+public class HotelController {
+
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping("/hotels")
+    public ResponseEntity<List<Hotel>> findAll() {
+
+        return ResponseEntity.ok(hotelService.findAllHotels());
+    }
+}
