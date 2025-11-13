0c2a10993eed19773808eb35594de75013ce9ec8
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2025-11-13 16:55:59 +0100

    Update test to fix db dependency

--- backend/src/test/java/com/urjc/plushotel/PlushotelApplicationTests.java
+++ backend/src/test/java/com/urjc/plushotel/PlushotelApplicationTests.java
@@ -1,13 +1,22 @@
 package com.urjc.plushotel;
 
+import com.urjc.plushotel.repositories.HotelRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 class PlushotelApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockitoBean
+    private HotelRepository hotelRepository;
+
+    @Test
+    void contextLoads() {
+    }
 
 }
