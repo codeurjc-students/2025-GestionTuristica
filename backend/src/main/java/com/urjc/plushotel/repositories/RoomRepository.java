0ee854fe19bb94380d42d5b7065e01b731794602
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2025-11-23 10:50:43 +0100

    Add @Repository to RoomRepository

--- backend/src/main/java/com/urjc/plushotel/repositories/RoomRepository.java
+++ backend/src/main/java/com/urjc/plushotel/repositories/RoomRepository.java
@@ -2,8 +2,12 @@ package com.urjc.plushotel.repositories;
 
 import com.urjc.plushotel.entities.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
-	Optional<Room> findByIdAndHotel_Slug(Long id, String slug);
+    
+    Optional<Room> findByIdAndHotel_Slug(Long id, String slug);
 }
