aa6a7d1792013a63e3fa5267805868f13ceb621e
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2025-11-23 11:27:29 +0100

    Configure to include new component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HotelListComponent } from './component/hotel-list.component';
+import { HotelCreateComponent } from './component/hotel-create.component';
 
 export const routes: Routes = [
-    {path: '', component: HotelListComponent}
+    { path: '', component: HotelListComponent },
+    { path: 'hotels/create', component: HotelCreateComponent }
 ];
