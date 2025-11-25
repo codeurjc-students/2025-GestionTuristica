7c8c39c6cc6a02cff3e26ba9630a3194fa989521
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2025-11-25 18:24:43 +0100

    Implement test for hotel creation

--- backend/src/test/java/com/urjc/plushotel/services/HotelServiceTest.java
+++ backend/src/test/java/com/urjc/plushotel/services/HotelServiceTest.java
@@ -1,6 +1,7 @@
 package com.urjc.plushotel.services;
 
 import com.urjc.plushotel.entities.Hotel;
+import com.urjc.plushotel.entities.Room;
 import com.urjc.plushotel.repositories.HotelRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -24,7 +27,6 @@ class HotelServiceTest {
 
     @Test
     void findAllTest() {
-
         Hotel h1 = Hotel.builder().name("H1").description("Hotel1 desc").country("España").city("Madrid").address("C/" +
                 " Example 4, Madrid").stars(3).slug("h1").build();
         Hotel h2 = Hotel.builder().name("H2").description("Hotel2 desc").country("España").city("Barcelona").address(
@@ -41,5 +43,25 @@ class HotelServiceTest {
         assertEquals(h2, result.getLast());
 
         verify(hotelRepository, times(1)).findAll();
+    }
+
+    @Test
+    void createHotelTest() {
+        Hotel h1 = Hotel.builder().name("H1").description("Hotel1 desc").country("España").city("Madrid").address("C/" +
+                " Example 4, Madrid").stars(3).slug("h1").rooms(new ArrayList<>()).build();
+
+
+        Room room1 = Room.builder().name("Room 1").description("").price(BigDecimal.ONE).available(true).build();
+        Room room2 = Room.builder().name("Room 2").description("").price(BigDecimal.TWO).available(true).build();
+
+        h1.getRooms().add(room1);
+        h1.getRooms().add(room2);
+
+        hotelService.createHotel(h1);
+
+        assertEquals(room1.getHotel(), h1);
+        assertEquals(room2.getHotel(), h1);
+
+        verify(hotelRepository, times(1)).save(h1);
     }
 }
