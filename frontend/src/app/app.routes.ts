5cc01c6f9c3e3fadaafc4b2ca6c3e2a5a3bf31bc
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2025-12-08 10:22:33 +0100

    Add edit component to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HotelListComponent } from './component/hotel-list.component';
 import { HotelCreateComponent } from './component/hotel-create.component';
+import { HotelEdit } from './component/hotel-edit.component';
 
 export const routes: Routes = [
     { path: '', component: HotelListComponent },
-    { path: 'hotels/create', component: HotelCreateComponent }
+    { path: 'hotels/create', component: HotelCreateComponent },
+    { path: 'hotels/edit/:slug', component: HotelEdit }
 ];
