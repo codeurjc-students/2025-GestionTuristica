2f336120293fb7b564ecdb44ab6c2786ed37995f
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2025-12-08 10:25:03 +0100

    Add button to access edit in hotel list

--- frontend/src/app/component/hotel-list.component.ts
+++ frontend/src/app/component/hotel-list.component.ts
@@ -1,19 +1,20 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { Hotel, HotelService } from "../services/hotel.service";
+import { RouterLink } from "@angular/router";
 
 
 @Component({
     selector: 'hotel-list',
     standalone: true,
-    imports: [ CommonModule ],
+    imports: [CommonModule, RouterLink],
     templateUrl: './hotel-list.component.html'
 })
 export class HotelListComponent implements OnInit{
 
     hotels: Hotel[] = []
 
-    constructor(private hotelService: HotelService){}
+    constructor(private readonly hotelService: HotelService){}
 
     ngOnInit() {
         this.hotelService.getHotels().subscribe({
