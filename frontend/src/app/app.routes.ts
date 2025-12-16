609d4b3df565f6a05be2001c60d762ff03a25adb
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2025-12-16 13:05:47 +0100

    Add access to hotel details

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HotelListComponent } from './component/hotel-list.component';
 import { HotelCreateComponent } from './component/hotel-create.component';
 import { HotelEdit } from './component/hotel-edit.component';
+import { HotelDetail } from './component/hotel-detail/hotel-detail';
 
 export const routes: Routes = [
     { path: '', component: HotelListComponent },
     { path: 'hotels/create', component: HotelCreateComponent },
-    { path: 'hotels/edit/:slug', component: HotelEdit }
+    { path: 'hotels/edit/:slug', component: HotelEdit },
+    { path: 'hotels/detail/:slug', component: HotelDetail }
 ];
