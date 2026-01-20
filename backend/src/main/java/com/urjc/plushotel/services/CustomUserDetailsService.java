5f03d38f188152b90ee4d29e72f46d2020bf5087
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-01-20 18:37:07 +0100

    Add CustomUserDetailsService

---
+++ backend/src/main/java/com/urjc/plushotel/services/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.urjc.plushotel.services;
+
+import com.urjc.plushotel.entities.User;
+import com.urjc.plushotel.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("The email introduced doesn't belong to any user")
+        );
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
