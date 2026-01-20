33e23d61fa048d16ec3f37fb0e7317d7a52108fb
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-01-20 18:42:12 +0100

    Implement login and rename service

---
+++ backend/src/main/java/com/urjc/plushotel/services/AuthService.java
@@ -0,0 +1,69 @@
+package com.urjc.plushotel.services;
+
+import com.urjc.plushotel.dtos.request.LoginRequest;
+import com.urjc.plushotel.dtos.request.RegisterRequest;
+import com.urjc.plushotel.dtos.response.LoginTokenDTO;
+import com.urjc.plushotel.dtos.response.UserDTO;
+import com.urjc.plushotel.entities.User;
+import com.urjc.plushotel.exceptions.EmailAlreadyRegisteredException;
+import com.urjc.plushotel.repositories.UserRepository;
+import com.urjc.plushotel.utils.Role;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService userDetailsService;
+    private final JwtService jwtService;
+
+    public AuthService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder,
+                       AuthenticationManager authenticationManager,
+                       CustomUserDetailsService userDetailsService,
+                       JwtService jwtService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtService = jwtService;
+    }
+
+    public UserDTO registerUser(RegisterRequest registerRequest) {
+        boolean registeredEmail = userRepository.existsByEmail(registerRequest.getEmail());
+        if (registeredEmail) {
+            throw new EmailAlreadyRegisteredException("This email is already registered");
+        }
+
+        User newUser = User.builder()
+                .name(registerRequest.getName())
+                .email(registerRequest.getEmail())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(Role.USER).build();
+
+        User user = userRepository.save(newUser);
+
+        return new UserDTO(user.getId(), user.getName(), user.getEmail(), user.getRole());
+    }
+
+    public LoginTokenDTO authenticate(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+
+        String token = jwtService.generateToken(userDetails);
+
+        return new LoginTokenDTO(token);
+    }
+}
