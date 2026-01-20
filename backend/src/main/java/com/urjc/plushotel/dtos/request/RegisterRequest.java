2618745c49601454b3913ff6f56d9a80603ee108
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-01-20 18:41:18 +0100

    Add new name field to User

--- backend/src/main/java/com/urjc/plushotel/dtos/request/RegisterRequest.java
+++ backend/src/main/java/com/urjc/plushotel/dtos/request/RegisterRequest.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class RegisterRequest {
 
+    private String name;
     private String email;
     private String password;
 }
--- backend/src/main/java/com/urjc/plushotel/dtos/response/UserDTO.java
+++ backend/src/main/java/com/urjc/plushotel/dtos/response/UserDTO.java
@@ -9,6 +9,7 @@ import lombok.Data;
 public class UserDTO {
 
     private Long id;
+    private String name;
     private String email;
     private Role role;
 }
