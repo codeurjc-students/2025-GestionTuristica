88e771a2e134deac01b2dcfb9c06e77e1283370e
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-01-25 17:23:48 +0100

    Create register component

---
+++ frontend/src/app/component/register/register.ts
@@ -0,0 +1,36 @@
+import { AuthService, RegisterRequest } from './../../services/auth.service';
+import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.html',
+  styleUrl: './register.scss',
+})
+export class Register {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
+  register() {
+    const registerData: RegisterRequest = {
+      name: this.name,
+      email: this.email,
+      password: this.password
+    }
+
+    this.authService.register(registerData).subscribe({
+      next: () => {
+        console.log('Registration successful');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => console.error('Registration error:', err)
+    })
+  }
+}
