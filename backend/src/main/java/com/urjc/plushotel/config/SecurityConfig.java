271df66383060909fc3456ef5788476dfb5abdc6
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-02-01 16:40:58 +0100

    Add PATCH and OPTION methods to CORS

--- backend/src/main/java/com/urjc/plushotel/config/SecurityConfig.java
+++ backend/src/main/java/com/urjc/plushotel/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
