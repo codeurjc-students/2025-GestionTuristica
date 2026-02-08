7a547637a24eb4782154874e4fb8c756062dd3cf
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-02-08 16:35:57 +0100

    Add @PreAuthorize to requests methods

--- backend/src/main/java/com/urjc/plushotel/controllers/ReservationChangeRequestController.java
+++ backend/src/main/java/com/urjc/plushotel/controllers/ReservationChangeRequestController.java
@@ -5,6 +5,7 @@ import com.urjc.plushotel.dtos.request.RequestFilter;
 import com.urjc.plushotel.dtos.response.ModificationRequestDTO;
 import com.urjc.plushotel.services.ReservationChangeRequestService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,24 +20,28 @@ public class ReservationChangeRequestController {
         this.reservationChangeRequestService = reservationChangeRequestService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/requests")
     public ResponseEntity<List<ModificationRequestDTO>> findReservationChangeRequest(@RequestParam(name = "status",
             defaultValue = "PENDING") RequestFilter status) {
         return ResponseEntity.ok(reservationChangeRequestService.findReservationChangeRequests(status));
     }
 
+    @PreAuthorize("hasRole('USER')")
     @PostMapping("/requests")
     public ResponseEntity<Void> createRequest(@RequestBody ModificationRequest request) {
         reservationChangeRequestService.createRequest(request);
         return ResponseEntity.ok().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/requests/approve/{requestId}")
     public ResponseEntity<Void> approveRequest(@PathVariable Long requestId) {
         reservationChangeRequestService.approveRequest(requestId);
         return ResponseEntity.ok().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/requests/reject/{requestId}")
     public ResponseEntity<Void> rejectRequest(@PathVariable Long requestId) {
         reservationChangeRequestService.rejectRequest(requestId);
