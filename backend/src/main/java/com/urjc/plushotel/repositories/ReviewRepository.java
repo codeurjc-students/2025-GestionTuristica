148d729fa67bb61cc1e71fb8c2c5f9d85d1735bc
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-02-12 17:25:06 +0100

    Create basic review repository, service and controller

---
+++ backend/src/main/java/com/urjc/plushotel/repositories/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.urjc.plushotel.repositories;
+
+import com.urjc.plushotel.entities.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
