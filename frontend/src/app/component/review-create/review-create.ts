b9b93c93af59d84c56ce49e6bec12c9ab68c8136
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-03-04 16:47:45 +0100

    Add compoennt to create review

---
+++ frontend/src/app/component/review-create/review-create.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { ReviewService, ReviewRequest } from './../../services/review.service';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from "@angular/forms";
+
+@Component({
+  selector: 'app-review-create',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './review-create.html'
+})
+export class ReviewCreate {
+
+  message: string = "";
+  rating: number = 1;
+
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly reviewService: ReviewService,
+    private readonly router: Router
+  ){}
+
+  createReview() {
+    const reservationIdentifier = this.activatedRoute.snapshot.paramMap.get('reservationIdentifier');
+    const request: ReviewRequest = {
+      message: this.message,
+      reservationIdentifier: reservationIdentifier || '',
+      rating: this.rating
+    };
+    this.reviewService.createReview(request).subscribe({
+      next: () => {
+        this.router.navigate(['/reviews']);
+      },
+      error: (error) => console.error(error)
+    })
+  }
+}
