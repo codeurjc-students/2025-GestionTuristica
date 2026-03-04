4e6162db07f25f21121ba3efe8a26e6de53fdf81
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-03-04 16:21:54 +0100

    Update service to use the new repository methods

--- backend/src/main/java/com/urjc/plushotel/services/RoomService.java
+++ backend/src/main/java/com/urjc/plushotel/services/RoomService.java
@@ -1,8 +1,11 @@
 package com.urjc.plushotel.services;
 
+import com.urjc.plushotel.dtos.response.RoomAvgRatingDTO;
 import com.urjc.plushotel.entities.Room;
 import com.urjc.plushotel.repositories.RoomRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomService {
@@ -13,9 +16,19 @@ public class RoomService {
         this.roomRepository = roomRepository;
     }
 
-    public Room getRoomById(Long id) {
+    public RoomAvgRatingDTO getRoomById(Long id) {
+        return roomRepository.findRoomWithAverageRatingById(id).orElseThrow(
+                () -> new RuntimeException("There is no room with such id")
+        );
+    }
+
+    public Room getRoomEntityById(Long id) {
         return roomRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("There is no room with such id")
         );
+    }
+
+    public List<RoomAvgRatingDTO> getRoomsByHotelId(Long hotelId) {
+        return roomRepository.findRoomsWithAverageRating(hotelId);
     }
 }
