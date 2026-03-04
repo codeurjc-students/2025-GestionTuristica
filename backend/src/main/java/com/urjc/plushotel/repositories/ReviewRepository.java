af5de45b383ecd94a1c3fd0e019ac2269ca4495b
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-03-04 16:16:53 +0100

    Add methods to recover reviews from db

--- backend/src/main/java/com/urjc/plushotel/repositories/ReviewRepository.java
+++ backend/src/main/java/com/urjc/plushotel/repositories/ReviewRepository.java
@@ -2,8 +2,30 @@ package com.urjc.plushotel.repositories;
 
 import com.urjc.plushotel.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findByReservationRoomId(Long roomId);
+
+    @Query("""
+            SELECT AVG(r.rating)
+            FROM Review r
+            WHERE r.reservation.room.hotel.id = :hotelId
+            """)
+    Double findAverageRatingByHotelId(Long hotelId);
+
+    @Query("""
+            SELECT AVG(r.rating)
+            FROM Review r
+            WHERE r.reservation.room.id = :roomId
+            """)
+    Double findAverageRatingByRoomId(Long roomId);
+
+    Optional<Review> findByReservationReservationIdentifier(String reservationIdentifier);
 }
