7edc2a42c2aa8df046d6f50d3f76d355af932355
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-03-18 16:31:48 +0100

    Update RoomService tests

--- backend/src/test/java/com/urjc/plushotel/services/RoomServiceTest.java
+++ backend/src/test/java/com/urjc/plushotel/services/RoomServiceTest.java
@@ -1,6 +1,7 @@
 package com.urjc.plushotel.services;
 
 import com.urjc.plushotel.dtos.response.RoomAvgRatingDTO;
+import com.urjc.plushotel.entities.Room;
 import com.urjc.plushotel.repositories.RoomRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,10 +10,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
@@ -37,5 +38,52 @@ class RoomServiceTest {
         assertEquals(roomDTO.getName(), resultRoom.getName());
         assertEquals(roomDTO.getDescription(), resultRoom.getDescription());
         assertEquals(roomDTO.getPrice(), resultRoom.getPrice());
+    }
+
+    @Test
+    void getRoomByIdNotFound() {
+        when(roomRepository.findRoomWithAverageRatingById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, () -> roomService.getRoomById(1L));
+    }
+
+    @Test
+    void getRoomEntityById() {
+        Room room = Room.builder().id(1L).name("Room 1").description("").price(BigDecimal.ONE).build();
+        when(roomRepository.findById(anyLong())).thenReturn(Optional.of(room));
+
+        Room resultRoom = roomService.getRoomEntityById(1L);
+
+        assertNotNull(resultRoom);
+        assertEquals(room.getId(), resultRoom.getId());
+        assertEquals(room.getName(), resultRoom.getName());
+        assertEquals(room.getDescription(), resultRoom.getDescription());
+        assertEquals(room.getPrice(), resultRoom.getPrice());
+    }
+
+    @Test
+    void getRoomEntityByIdNotFound() {
+        when(roomRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(RuntimeException.class, () -> roomService.getRoomEntityById(1L));
+    }
+
+    @Test
+    void getRoomsByHotelId() {
+        RoomAvgRatingDTO roomDTO1 = new RoomAvgRatingDTO(1L, "room1", "desc1", BigDecimal.TEN, 3.3);
+        RoomAvgRatingDTO roomDTO2 = new RoomAvgRatingDTO(2L, "room2", "desc2", BigDecimal.TWO, 3.7);
+        when(roomRepository.findRoomsWithAverageRating(anyLong())).thenReturn(List.of(roomDTO1, roomDTO2));
+
+        List<RoomAvgRatingDTO> result = roomService.getRoomsByHotelId(1L);
+
+        assertNotNull(result);
+        assertEquals(roomDTO1.getId(), result.getFirst().getId());
+        assertEquals(roomDTO2.getId(), result.getLast().getId());
+        assertEquals(roomDTO1.getName(), result.getFirst().getName());
+        assertEquals(roomDTO2.getName(), result.getLast().getName());
+        assertEquals(roomDTO1.getDescription(), result.getFirst().getDescription());
+        assertEquals(roomDTO2.getDescription(), result.getLast().getDescription());
+        assertEquals(roomDTO1.getPrice(), result.getFirst().getPrice());
+        assertEquals(roomDTO2.getPrice(), result.getLast().getPrice());
     }
 }
