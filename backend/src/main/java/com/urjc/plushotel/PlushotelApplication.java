5da24f73bfffd180cded30bf5f4e11ee20dace1a
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-04-20 19:23:06 +0200

    Add @EnableAsync

--- backend/src/main/java/com/urjc/plushotel/PlushotelApplication.java
+++ backend/src/main/java/com/urjc/plushotel/PlushotelApplication.java
@@ -2,12 +2,14 @@ package com.urjc.plushotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PlushotelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlushotelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlushotelApplication.class, args);
+    }
 
 }
