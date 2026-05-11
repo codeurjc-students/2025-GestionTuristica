fb4bc0a3e540ef1eebd2099ce8618a7bb27c3dd5
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-05-11 19:45:26 +0200

    Add necessary ImageRepository methods

--- backend/src/main/java/com/urjc/plushotel/repositories/ImageRepository.java
+++ backend/src/main/java/com/urjc/plushotel/repositories/ImageRepository.java
@@ -3,5 +3,15 @@ package com.urjc.plushotel.repositories;
 import com.urjc.plushotel.entities.HotelImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepository extends JpaRepository<HotelImage, Long> {
+
+    List<HotelImage> findByHotel_SlugOrderByPosition(String slug);
+
+    List<HotelImage> findByRoom_IdOrderByPosition(Long roomId);
+
+    List<HotelImage> findByRoomIsNullAndPosition(int position);
+
+    List<HotelImage> findByHotel_SlugAndRoomIsNotNullAndPosition(String slug, int position);
 }
