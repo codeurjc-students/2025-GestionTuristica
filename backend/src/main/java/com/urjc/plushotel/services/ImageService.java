a08067924f6593a43caecdd2fa84d9fa1ec452bf
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-05-11 19:46:12 +0200

    Add methods to obtain images to ImageService

--- backend/src/main/java/com/urjc/plushotel/services/ImageService.java
+++ backend/src/main/java/com/urjc/plushotel/services/ImageService.java
@@ -1,11 +1,14 @@
 package com.urjc.plushotel.services;
 
+import com.urjc.plushotel.dtos.response.HotelImageDTO;
 import com.urjc.plushotel.entities.Hotel;
 import com.urjc.plushotel.entities.HotelImage;
 import com.urjc.plushotel.entities.Room;
 import com.urjc.plushotel.repositories.ImageRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 public class ImageService {
@@ -47,5 +50,42 @@ public class ImageService {
         image = imageRepository.save(image);
 
         return image;
+    }
+
+    public List<HotelImageDTO> getImagesByHotelSlug(String slug) {
+
+        List<HotelImage> images = imageRepository.findByHotel_SlugOrderByPosition(slug);
+
+        return images.stream().map(
+                img -> new HotelImageDTO(minioService.getImageUrl(img.getFileName()), img.getHotel().getId())
+        ).toList();
+    }
+
+    public List<HotelImageDTO> getImagesByRoomId(Long roomId) {
+
+        List<HotelImage> images = imageRepository.findByRoom_IdOrderByPosition(roomId);
+
+        return images.stream().map(
+                img -> new HotelImageDTO(minioService.getImageUrl(img.getFileName()), img.getRoom().getId())
+        ).toList();
+    }
+
+    public List<HotelImageDTO> getHotelsMainImages() {
+
+        List<HotelImage> images = imageRepository.findByRoomIsNullAndPosition(0);
+
+        return images.stream().map(
+                img -> new HotelImageDTO(minioService.getImageUrl(img.getFileName()), img.getHotel().getId())
+        ).toList();
+    }
+
+    public List<HotelImageDTO> getHotelRoomsMainImages(String slug) {
+
+        List<HotelImage> images = imageRepository.findByHotel_SlugAndRoomIsNotNullAndPosition(slug, 0);
+
+        return images.stream().map(
+                img -> new HotelImageDTO(minioService.getImageUrl(img.getFileName()), img.getHotel().getId(),
+                        img.getRoom().getId())
+        ).toList();
     }
 }
