30e4ab3c955e3e0a298b042add526a004c283900
PabloSanchezMa <p.sanchezma.2020@alumnos.urjc.es>
2026-06-01 20:02:02 +0200

    Add h2 to load context with local database

--- backend/src/test/java/com/urjc/plushotel/PlushotelApplicationTests.java
+++ backend/src/test/java/com/urjc/plushotel/PlushotelApplicationTests.java
@@ -5,14 +5,12 @@ import com.urjc.plushotel.services.AuthService;
 import com.urjc.plushotel.services.CustomUserDetailsService;
 import com.urjc.plushotel.services.JwtService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@ActiveProfiles("test")
 class PlushotelApplicationTests {
 
     @MockitoBean
